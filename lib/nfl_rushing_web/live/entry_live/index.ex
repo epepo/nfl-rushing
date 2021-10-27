0f10c8c614b2a92232da99b080366f0a91553427
epepo <epoernbacher@icloud.com>
2021-10-27 00:40:22 -0300

    Add a simplified player name filtering to Entries table

--- lib/nfl_rushing_web/live/entry_live/index.ex
+++ lib/nfl_rushing_web/live/entry_live/index.ex
@@ -8,10 +8,33 @@ defmodule NFLRushingWeb.EntryLive.Index do
 
   alias NFLRushing.Stats
 
+  require Logger
+
   @impl true
   def mount(_params, _session, socket) do
-    socket = assign(socket, entries: Stats.list_entries())
+    socket =
+      socket
+      |> assign(entries: Stats.list_entries())
+      |> assign(player_filter: "")
 
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event(event, unsigmned_params, socket)
+
+  def handle_event("update_filters", %{"filters" => %{"player" => player}}, socket) do
+    socket =
+      socket
+      |> assign(entries: Stats.list_entries(filters: %{player: player}))
+      |> assign(player_filter: player)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("export", _params, socket) do
+    Logger.info("Export action called")
+
+    {:noreply, socket}
   end
 end
--- test/nfl_rushing_web/live/entry_live_test.exs
+++ test/nfl_rushing_web/live/entry_live_test.exs
@@ -19,5 +19,19 @@ defmodule NFLRushingWeb.EntryLiveTest do
       assert html =~ "Player"
       assert html =~ entry.player
     end
+
+    test "allows filtering by player name", %{conn: conn, entry: entry} do
+      other_entry = entry_fixture(player: "Some Other Player")
+
+      {:ok, live, _html} = live(conn, Routes.entry_index_path(conn, :index))
+
+      html =
+        live
+        |> form("#filters", filters: %{player: entry.player})
+        |> render_change()
+
+      assert html =~ entry.player
+      refute html =~ other_entry.player
+    end
   end
 end
