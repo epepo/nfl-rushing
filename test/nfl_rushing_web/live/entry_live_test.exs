2f178ee8413ace13e958d817b2d6d7b41ab5795a
epepo <epoernbacher@icloud.com>
2021-10-27 22:50:42 -0300

    Allow sorting by any column on the Entries table

--- test/nfl_rushing_web/live/entry_live_test.exs
+++ test/nfl_rushing_web/live/entry_live_test.exs
@@ -4,34 +4,45 @@ defmodule NFLRushingWeb.EntryLiveTest do
   import Phoenix.LiveViewTest
   import NFLRushing.StatsFixtures
 
-  defp create_entry(_context) do
-    entry = entry_fixture()
-
-    %{entry: entry}
-  end
-
   describe "index" do
-    setup [:create_entry]
+    test "lists all entries", %{conn: conn} do
+      entry = entry_fixture()
 
-    test "lists all entries", %{conn: conn, entry: entry} do
       {:ok, _index_live, html} = live(conn, Routes.entry_index_path(conn, :index))
 
       assert html =~ "Player"
       assert html =~ entry.player
     end
 
-    test "allows filtering by player name", %{conn: conn, entry: entry} do
+    test "allows filtering by player name", %{conn: conn} do
+      target_entry = entry_fixture(player: "Target Player")
       other_entry = entry_fixture(player: "Some Other Player")
 
       {:ok, live, _html} = live(conn, Routes.entry_index_path(conn, :index))
 
       html =
         live
-        |> form("#filters", filters: %{player: entry.player})
+        |> form("#filters", filters: %{player: target_entry.player})
         |> render_change()
 
-      assert html =~ entry.player
+      assert html =~ target_entry.player
       refute html =~ other_entry.player
+    end
+
+    test "allows sorting by some column", %{conn: conn} do
+      second_entry = entry_fixture(rushing_attempts: 10)
+      first_entry = entry_fixture(rushing_attempts: 5)
+      last_entry = entry_fixture(rushing_attempts: 12)
+
+      {:ok, live, _html} = live(conn, Routes.entry_index_path(conn, :index))
+
+      live
+      |> element(~s|*[phx-value-column="rushing_attempts"]|)
+      |> render_click()
+
+      assert has_element?(live, "tbody > tr:nth-child(1) > td:first-child()", first_entry.player)
+      assert has_element?(live, "tbody > tr:nth-child(2) > td:first-child()", second_entry.player)
+      assert has_element?(live, "tbody > tr:nth-child(3) > td:first-child()", last_entry.player)
     end
   end
 end
